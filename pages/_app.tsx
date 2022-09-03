9f9830c4456b730d1266558fafe21fc2b6b3715f
Hynek Fišera <hynek.fisera@seznam.cz>
2022-09-03 09:27:25 +0200

    Fix icon bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
+import "@fortawesome/fontawesome-svg-core/styles";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
 import SEO from "../next-seo.config";
 import Header from "../components/Header";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
