ca545e066bef693624dbd7922f1d6efc304f0941
Hynek Fišera <hynek.fisera@seznam.cz>
2022-12-04 15:40:15 +0100

    Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,12 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GoogleAnalytics trackPageViews />
       <DefaultSeo {...SEO} />
       <Header />
       <Component {...pageProps} />
